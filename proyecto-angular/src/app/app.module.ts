a44666808af4470a5fda3ffabeb23193928ed920
juan Mazzaforte <juanmazzaforte17@gmail.com>
2022-08-12 17:48:08 -0300

    index to angular

---
+++ proyecto-angular/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { BannerComponent } from './banner/banner.component';
+import { LoginComponent } from './modals/login/login.component';
+import { RedesComponent } from './redes/redes.component';
+import { ButtonLoginComponent } from './button-login/button-login.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    AboutmeComponent,
+    BannerComponent,
+    LoginComponent,
+    RedesComponent,
+    ButtonLoginComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
